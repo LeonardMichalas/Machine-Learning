e5584afb73fa830edc2fb34489ebf43cb8163ab6
LeonardMichalas <leonard.michalas@hpe.com>
2018-04-20 15:07:35 +0200

    added a simple backend

---
+++ webapp.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/getvisitors")
+def nice():
+    return json.dumps({"Typ":"Mensch", "Anzahl": 13, "Alter": 4}) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
