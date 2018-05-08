8a64cad37eada26eb6e5803aaa9ee078457706e7
LeonardMichalas <leonard.michalas@hpe.com>
2018-05-08 12:09:14 +0200

    some changes

--- iris.py
+++ iris.py
@@ -18,7 +18,6 @@ train_target = np.delete(iris.target, test_idx)
 train_data = np.delete(iris.data, test_idx, axis=0)
 
 #testing data
-
 test_target = iris.target[test_idx]
 test_data = iris.data[test_idx]
 
